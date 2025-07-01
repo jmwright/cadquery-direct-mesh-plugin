19af2c4e8aaceae17e695726f0981a4c3c166724
Jeremy Wright <wrightjmf@gmail.com>
2025-07-01 13:20:09 -0400

    Trying to get CI linting working and added CI test structure

---
+++ tests/test_assemblies.py
@@ -0,0 +1,16 @@
+import cadquery as cq
+import cadquery_direct_mesh_plugin
+
+def test_basic_assembly():
+    """
+    Tests to make sure a basic assembly will work.
+    """
+
+    # Create a simple test assembly
+    assy = cq.Assembly()
+    assy.add(cq.Workplane().box(10, 10, 10), name="box_1")
+
+    mesh = assy.toMesh()
+
+    # Make sure we have the correct number of vertices
+    assert len(mesh["vertices"]) == 24
